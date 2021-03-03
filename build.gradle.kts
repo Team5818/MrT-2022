bea18f2949549078e66c928eb71658f4157ceaaa
Boomaa23 <cmo93003@yahoo.com>
2021-03-02 23:13:10 -0800

    Update 5818-lib to 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.rivierarobotics:5818-lib:0.1.0")
+    implementation("org.rivierarobotics:5818-lib:0.1.1")
     implementation("org.rivierarobotics.apparjacktus:apparjacktus:0.1.1")
     commonLib("net.octyl.apt-creator", "apt-creator", "0.1.4") {
         compileOnly(lib("annotations"))
