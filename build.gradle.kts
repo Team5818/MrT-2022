2ae191dc447ec947ccc58b39542057ce39584109
Boomaa23 <cmo93003@yahoo.com>
2021-05-16 03:38:41 -0700

    Bump 5818-lib to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.rivierarobotics:5818-lib:0.1.4")
+    implementation("org.rivierarobotics:5818-lib:0.2.0")
     implementation("org.rivierarobotics.apparjacktus:apparjacktus:0.1.1")
     commonLib("net.octyl.apt-creator", "apt-creator", "0.1.4") {
         compileOnly(lib("annotations"))
--- src/main/java/org/rivierarobotics/inject/Input.java
+++ src/main/java/org/rivierarobotics/inject/Input.java
@@ -37,9 +37,4 @@ public @interface Input {
         RIGHT_JS,
         BUTTONS
     }
-
-    @Qualifier
-    @interface Composite {
-        User value();
-    }
 }
