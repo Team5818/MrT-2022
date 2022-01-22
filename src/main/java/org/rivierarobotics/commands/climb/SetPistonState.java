8c494a350e03fc8a5c73d9e9f1e0584e73aca8b9
MrPizzaBagel <zombiegam3r2o06@gmail.com>
2022-01-22 13:36:58 -0800

    variables in SetPositionState changed to final properly

--- src/main/java/org/rivierarobotics/commands/climb/SetPistonState.java
+++ src/main/java/org/rivierarobotics/commands/climb/SetPistonState.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of Placeholder-2022, licensed under the GNU General Public License (GPLv3).
+ *
+ * Copyright (c) Riviera Robotics <https://github.com/Team5818>
+ * Copyright (c) contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.rivierarobotics.commands.climb;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,7 +28,7 @@ public class SetPistonState extends CommandBase {
 
     private final Pistons piston;
     private final boolean isOpen;
-    private final PistonControl pistonControl;
+    private PistonControl pistonControl;
 
     public SetPistonState(Pistons piston, boolean isOpen) {
         this.piston = piston;
