8c1ae18634ab9718a00ed55511c1684c685dfe14
Aras Janusonis <aras@janusonis.net>
2022-02-07 19:16:50 -0800

    limelight stuff deez

---
+++ src/main/java/org/rivierarobotics/commands/LLCommands/TrackGoal.java
@@ -0,0 +1,9 @@
+package org.rivierarobotics.commands.LLCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TrackGoal extends CommandBase {
+    private final Hood hood;
+
+
+}
