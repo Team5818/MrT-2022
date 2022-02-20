136c5258617cf998c25f2e97df62e62bd14787f4
MrPizzaBagel <zombiegam3r2o06@gmail.com>
2022-02-19 16:37:07 -0800

    photon vision

--- src/main/java/org/rivierarobotics/subsystems/vision/Limelight.java
+++ src/main/java/org/rivierarobotics/subsystems/vision/Limelight.java
@@ -2,6 +2,7 @@ package org.rivierarobotics.subsystems.vision;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
@@ -22,23 +23,27 @@ public class Limelight extends SubsystemBase {
     }
 
     public double getTy() {
-        return limelightTable.getEntry("ty").getDouble(-1);
+        return limelightTable.getEntry("targetPitch").getDouble(0);
     }
 
     public double getTx() {
-        return limelightTable.getEntry("tx").getDouble(-1);
-    }
-
-    public double getArea(){
-        return limelightTable.getEntry("ta").getDouble(-1);
+        return limelightTable.getEntry("targetYaw").getDouble(0);
     }
 
     public boolean getDetected(){
-        return limelightTable.getEntry("tv").getDouble(-1) == 0;
+        return limelightTable.getEntry("hasTarget").getBoolean(false);
     }
 
     public double getDistance(){
         return Math.atan(Math.toRadians(getTy()))*(ShooterConstants.getGoalHeight() - ShooterConstants.getRobotHeight());
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("lly", getTy());
+        SmartDashboard.putNumber("llx", getTx());
+        SmartDashboard.putBoolean("detected", getDetected());
+        SmartDashboard.putNumber("Distance", getDistance());
     }
 
 
