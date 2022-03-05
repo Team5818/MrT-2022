604b679988ff0dc3ccd9e546a26af74ae8118a02
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-04 19:34:30 -0800

    wrote end method in collect command

--- src/main/java/org/rivierarobotics/commands/collect/Collect.java
+++ src/main/java/org/rivierarobotics/commands/collect/Collect.java
@@ -1,12 +1,13 @@
 package org.rivierarobotics.commands.collect;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.rivierarobotics.subsystems.intake.Intake;
 import org.rivierarobotics.util.ml.BoundingBox;
 import org.rivierarobotics.util.ml.MLCore;
 import org.rivierarobotics.util.ml.MLObject;
 
-public class Collect extends CommandBase{
+public class Collect extends CommandBase {
     private final Intake intake;
 
     public Collect() {
@@ -16,12 +17,25 @@ public class Collect extends CommandBase{
     @Override
     public void execute() {
         intake.setIntakeState(true);
-        intake.setVoltage(4);
+        intake.setIntakeVoltage(5);
+        intake.setBeltVoltage(5);
     }
 
     @Override
     public void end(boolean interrupted) {
-        intake.setIntakeState(!interrupted);
-        intake.setVoltage(0);
+        if(interrupted) {
+            intake.setIntakeState(false);
+            intake.setIntakeVoltage(0);
+            double initialTime = Timer.getFPGATimestamp();
+            double waitTime = 2;
+
+            intake.setIntakeVoltage(-1);
+            while(Timer.getFPGATimestamp() - initialTime < waitTime) {
+                // Do absolutely nothing
+            }
+            intake.setIntakeVoltage(0);
+        }
+
+
     }
 }
