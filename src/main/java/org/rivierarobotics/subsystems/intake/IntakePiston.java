c068c982facc84fd7d27305b3ee0d7d6bf74d69f
kiwi0401 <kiwi04011@gmail.com>
2022-03-12 20:09:51 -0800

    Path planner :D

--- src/main/java/org/rivierarobotics/subsystems/intake/IntakePiston.java
+++ src/main/java/org/rivierarobotics/subsystems/intake/IntakePiston.java
@@ -1,5 +1,6 @@
 package org.rivierarobotics.subsystems.intake;
 
+import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakePiston extends SubsystemBase {
--- src/main/java/org/rivierarobotics/util/swerve/TrajectoryFollower.java
+++ src/main/java/org/rivierarobotics/util/swerve/TrajectoryFollower.java
@@ -1,5 +1,6 @@
 package org.rivierarobotics.util.swerve;
 
+import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -16,6 +17,7 @@ import java.nio.file.Path;
 
 public class TrajectoryFollower {
     private final HolonomicDriveController holonomicDriveController;
+    private final PathPlannerTrajectory pathPlannerTrajectory;
     private final double startTime;
     private final PoseEstimator estimator;
     private final Trajectory trajectory;
@@ -34,24 +36,49 @@ public class TrajectoryFollower {
     }
 
     public TrajectoryFollower(Trajectory trajectory, boolean resetPose, Gyro gyro, DriveTrain driveTrain) {
+       this(trajectory, null, resetPose, gyro, driveTrain);
+    }
+
+    public TrajectoryFollower(PathPlannerTrajectory trajectory, boolean resetPose, Gyro gyro, DriveTrain driveTrain) {
+        this(null,trajectory,resetPose,gyro,driveTrain);
+    }
+
+    private TrajectoryFollower(Trajectory trajectory, PathPlannerTrajectory pathPlannerTrajectory,
+                               boolean resetPose, Gyro gyro, DriveTrain driveTrain) {
         this.holonomicDriveController = driveTrain.getHolonomicDriveController();
+        this.pathPlannerTrajectory = pathPlannerTrajectory;
         this.startTime = Timer.getFPGATimestamp();
         this.gyro = gyro;
         this.driveTrain = driveTrain;
         this.estimator = driveTrain.getPoseEstimator();
         this.trajectory = trajectory;
-        if(trajectory == null) {
+        if(trajectory == null && pathPlannerTrajectory == null) {
             DriverStation.reportError("Trying to drive null path", false);
             return;
         }
         if(resetPose) {
-            var transposedPose = new Pose2d(trajectory.getInitialPose().getTranslation(), gyro.getRotation2d());
-            estimator.resetPose(transposedPose);
+            estimator.resetPose(getPathTrajectory());
+        }
+    }
+
+    private Pose2d getPathTrajectory() {
+        if(trajectory != null) {
+            return new Pose2d(trajectory.getInitialPose().getTranslation(), gyro.getRotation2d());
+        } else {
+            return new Pose2d(pathPlannerTrajectory.getInitialPose().getTranslation(), gyro.getRotation2d());
+        }
+    }
+
+    private Trajectory.State sample(double t) {
+        if(trajectory != null) {
+            return trajectory.sample(t);
+        } else {
+            return pathPlannerTrajectory.sample(t);
         }
     }
 
     public boolean isFinished() {
-        return trajectory == null || (Timer.getFPGATimestamp() - this.startTime > this.trajectory.getTotalTimeSeconds());
+        return (trajectory == null && pathPlannerTrajectory == null) || (Timer.getFPGATimestamp() - this.startTime > this.trajectory.getTotalTimeSeconds());
     }
 
     /**
@@ -59,13 +86,13 @@ public class TrajectoryFollower {
      * returns false when path is done.
      */
     public void followController() {
-        if(trajectory == null) {
+        if(trajectory == null && pathPlannerTrajectory == null) {
             return;
         }
 
         double timePassed = Timer.getFPGATimestamp() - this.startTime;
 
-        var state = this.trajectory.sample(timePassed);
+        var state = sample(timePassed);
         var controls = holonomicDriveController.calculate(
                 new Pose2d(estimator.getRobotPose().getTranslation(), new Rotation2d(0)),
                 state,
