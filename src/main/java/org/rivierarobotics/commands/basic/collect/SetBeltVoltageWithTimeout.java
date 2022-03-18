e13827b680848ea21822bd0e98ec54f20a591030
kiwi0401 <kiwi04011@gmail.com>
2022-03-18 14:28:15 -0700

    fix crashing command

--- src/main/java/org/rivierarobotics/commands/basic/collect/SetBeltVoltageWithTimeout.java
+++ src/main/java/org/rivierarobotics/commands/basic/collect/SetBeltVoltageWithTimeout.java
@@ -1,5 +1,6 @@
 package org.rivierarobotics.commands.basic.collect;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.rivierarobotics.subsystems.intake.IntakeBelt;
 
@@ -7,21 +8,24 @@ public class SetBeltVoltageWithTimeout extends CommandBase {
 
     private final IntakeBelt intakeBelt;
     private final double voltage;
+    private final double timeout;
+    private double startTime = 0.0;
     public SetBeltVoltageWithTimeout(double voltage, double timeout) {
         this.intakeBelt = IntakeBelt.getInstance();
         this.voltage = voltage;
-        withTimeout(timeout);
+        this.timeout = timeout;
         addRequirements(intakeBelt);
     }
 
     @Override
     public void initialize() {
         intakeBelt.setBeltVoltage(voltage);
+        startTime = Timer.getFPGATimestamp();
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return Timer.getFPGATimestamp() - startTime >= timeout;
     }
 
     @Override
