73866affea354a1f985ef755847918955cda5100
Andrew-Ji10 <jiandrew10@gmail.com>
2022-04-02 23:07:16 -0700

    climb piston changes

--- src/main/java/org/rivierarobotics/subsystems/MotorIDs.java
+++ src/main/java/org/rivierarobotics/subsystems/MotorIDs.java
@@ -34,9 +34,12 @@ public class MotorIDs {
     public static final int BACK_RIGHT_DRIVE = 7;
     public static final int BACK_RIGHT_STEER = 8;
     //climb ids
-    public static final int SOLENOID_LOW = 5;
-    public static final int SOLENOID_MID = 3;
-    public static final int SOLENOID_HIGH = 4;
+    public static final int SOLENOID_LOW_OPEN = 3;
+    public static final int SOLENOID_LOW_CLOSED = 6;
+    public static final int SOLENOID_MID_OPEN = 2;
+    public static final int SOLENOID_MID_CLOSED = 5;
+    public static final int SOLENOID_HIGH_OPEN = 4;
+    public static final int SOLENOID_HIGH_CLOSED = 7;
     public static final int CLIMB_ROTATE_A = 10;
     public static final int CLIMB_ROTATE_B = 11;
     public static final int LOW_SWITCH_A = 1;
