1fda53fd07a0a7bee67a34a5cf0fb1c6a781fb9d
Andrew-Ji10 <jiandrew10@gmail.com>
2022-04-04 08:21:43 -0700

    2 ball leftside auto

---
+++ src/main/java/org/rivierarobotics/commands/auto/twoBallAuto.java
@@ -0,0 +1,22 @@
+package org.rivierarobotics.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import org.rivierarobotics.commands.advanced.collect.CollectBalls;
+import org.rivierarobotics.commands.advanced.drive.DrivePathPlannerPath;
+import org.rivierarobotics.commands.advanced.shoot.AutoAimShoot;
+import org.rivierarobotics.commands.basic.drive.SetDriveAngle;
+
+public class twoBallAuto extends SequentialCommandGroup {
+
+    public twoBallAuto(){
+        super(
+            new ParallelDeadlineGroup(
+                    new DrivePathPlannerPath("2BallLeft", 7,4 ),
+                    new CollectBalls()
+            ),
+            new SetDriveAngle(45),
+            new AutoAimShoot(true)
+        );
+    }
+}
