7f0e78561afbd4b3e6d7ed558c0fffc24d073368
Andrew-Ji10 <jiandrew10@gmail.com>
2022-04-07 07:52:00 -0700

    1 Ball ML

---
+++ src/main/java/org/rivierarobotics/commands/auto/MLCollect1.java
@@ -0,0 +1,29 @@
+package org.rivierarobotics.commands.auto;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import org.rivierarobotics.commands.advanced.collect.CollectBalls;
+import org.rivierarobotics.commands.advanced.drive.DriveToClosest;
+import org.rivierarobotics.commands.advanced.drive.RotateToTargetOffOfPose;
+import org.rivierarobotics.commands.advanced.shoot.AutoAimShoot;
+import org.rivierarobotics.commands.advanced.shoot.RotateBall;
+import org.rivierarobotics.commands.basic.collect.SetIntakeState;
+import org.rivierarobotics.robot.Logging;
+
+public class MLCollect1 extends SequentialCommandGroup{
+
+    public MLCollect1(boolean isRight) {
+        addCommands(
+                new SetIntakeState(true),
+                new ParallelDeadlineGroup(
+                        new SequentialCommandGroup(
+                                new FindBall(isRight), new RotateBall().withTimeout(1), new DriveToClosest().withTimeout(1).andThen(new WaitCommand(2))
+                        ).withInterrupt(() -> Timer.getFPGATimestamp() - Logging.autoStartTime >= 11),
+                        new CollectBalls()
+                ),
+                new RotateToTargetOffOfPose(),
+                new AutoAimShoot(true));
+    }
+}
